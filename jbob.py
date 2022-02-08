ab55cb0a91e7628928f419eda1b09381d482d29c
Martin Billinger <flkazemakase@gmail.com>
2022-02-08 14:06:18 +0100

    the overall structure takes form...

--- jbob.py
+++ jbob.py
@@ -1,32 +1,135 @@
-import re
+from dataclasses import dataclass
+from typing import Any
+
 from lark import Lark, Transformer
 
 
-def evaluate(expr):
+global_env = {}
+
+
+#  The Evaluator
+# ===============
+
+
+def evaluate(expr, env=None):
+    if env is None:
+        env = global_env
     if isinstance(expr, str):
-        tree = sexpr_parser.parse(expr)
-        expr = TreeToSexpr().transform(tree)
-        print(to_string(expr))
-    raise NotImplementedError()
+        expr = analyze(parse(expr))
+    return expr(env)
+
+
+def analyze(expr):
+    match expr:
+        case Pair("quote", Pair(x, ())):
+            return lambda _: x
+        case Pair("defun", Pair(name, Pair(args, Pair(body, ())))):
+            return analyze_definition(name, args, body)
+        case Pair("if", Pair(q, Pair(a, Pair(e, ())))):
+            raise NotImplementedError()
+        case Pair(f, args):
+            return analyze_application(f, args)
+        case str(symbol):
+            return analyze_reference(symbol)
+        case list([*x]):
+            return analyze_sequence(x)
+        case _:
+            raise NotImplementedError(to_string(expr))
+
+
+def analyze_application(f, args):
+    f_exec = analyze(f)
+    arg_execs = analyze_args(args)
+
+    def the_application(env):
+        f = f_exec(env)
+        args = map(lambda a: a(env), iter(arg_execs))
+        return f(*args)
+
+    return the_application
+
+
+def analyze_args(args):
+    if is_null(args):
+        return args
+    return cons(analyze(car(args)), analyze_args(cdr(args)))
+
+
+def analyze_reference(name):
+    return lambda env: env[name]
+
+
+def analyze_sequence(exprs):
+    execs = [analyze(x) for x in exprs]
+
+    def the_sequence(env):
+        result = None
+        for x in execs:
+            result = x(env)
+        return result
+
+    return the_sequence
+
+
+def analyze_definition(name, params, body):
+    body_exec = analyze(body)
+    param_str = ", ".join(map(str, iter(params)))
+    python_name = pythonize(name)
+
+    def the_definition(env):
+        glob = {}
+        exec(
+            f"def {python_name}({param_str}): return body_exec(global_env)",
+            {"body_exec": body_exec, "global_env": global_env},
+            glob,
+        )
+        global_env[name] = glob[python_name]
+
+    return the_definition
+
+
+def pythonize(name: str) -> str:
+    return (
+        name.replace("_", "__")
+        .replace("-", "_")
+        .replace("/", "_slash_")
+        .replace("+", "_plus_")
+        .replace("*", "_star_")
+    )
+
+
+#  The S-Expression Parser
+# =========================
+
+
+def parse(src):
+    tree = sexpr_parser.parse(src)
+    return TreeToSexpr().transform(tree)
 
 
 sexpr_parser = Lark(
     r"""
+    program: sexpr*
+    
     ?sexpr: SIGNED_NUMBER   -> number
          | SYMBOL           -> symbol
          | list
-         | quote
+         | quote         
          
-    quote : "'" sexpr
-    list : "(" sexpr* ")"    
+    quote: "'" sexpr
+    list: "(" sexpr* ")"    
          
-    SYMBOL : ("A".."Z" | "a".."z" | "-" | "/" | "_" | "." | "!")+
+    SYMBOL: ("A".."Z" | "a".."z" | "-" | "+" | "/" | "_" | "." | "!" | "?")+
+    
+    COMMENT: ";" /[^\n]*/ NEWLINE
+    %ignore COMMENT
     
     %import common.SIGNED_NUMBER
+    %import common.NEWLINE
     %import common.WS
     %ignore WS
 """,
-    start="sexpr",
+    start="program",
 )
 
 
@@ -49,25 +152,44 @@ class TreeToSexpr(Transformer):
             out = cons(x, out)
         return out
 
+    def program(self, prog):
+        return prog
+
+
+#  The Basics
+# ============
+
+
+@dataclass
+class Pair:
+    car: Any
+    cdr: Any
+
+    def __iter__(self):
+        pair = self
+        while is_pair(pair):
+            yield pair.car
+            pair = pair.cdr
+
 
 def is_null(obj):
     return obj == ()
 
 
 def is_pair(obj):
-    return isinstance(obj, tuple) and len(obj) == 2
+    return isinstance(obj, Pair)
 
 
 def cons(a, d):
-    return (a, d)
+    return Pair(a, d)
 
 
 def car(p):
-    return p[0]
+    return p.car
 
 
 def cdr(p):
-    return p[1]
+    return p.cdr
 
 
 def cadr(p):
@@ -86,7 +208,7 @@ def is_list(obj, len=None):
 
 def to_string(obj):
     if is_list(obj, len=2) and car(obj) == "quote":
-        return f"'{cadr(obj)}"
+        return f"'{to_string(cadr(obj))}"
     elif is_pair(obj):
         out = ["(", to_string(car(obj))]
         obj = cdr(obj)
@@ -103,11 +225,13 @@ def to_string(obj):
         return str(obj)
 
 
-tree = sexpr_parser.parse("( J-Bob/step 'x '42 123 456 foo)")
-sexpr = TreeToSexpr().transform(tree)
-print(tree.pretty())
-print(to_string(sexpr))
-print(to_string((1, (2, 3))))
+#  J-Bob
+# =======
 
+
+with open("j-bob.scm") as fd:
+    ast = parse(fd.read())
+program = analyze(ast)
+evaluate(program)
 
 evaluate("(J-Bob/step (prelude) '(car (cons 'ham '(cheese))) '())")
