97b367026b4d0e2492430eb4b2dc73aac8ca7183
Martin Billinger <mbillingr@users.noreply.github.com>
2022-02-15 20:12:39 +0100

    back to simple

--- jbob.py
+++ jbob.py
@@ -1,4 +1,3 @@
-from code_transform import inline_all
 from jbob_parser import parse
 from jbob_interpreter import analyze, evaluate, global_env
 
@@ -11,26 +10,6 @@ def undefine(name):
 # =======
 with open("j-bob.scm") as fd:
     ast = parse(fd.read())
-ast = inline_all(ast)
 
 program = analyze(ast)
 evaluate(program)
-
-
-# from jbob_compiler import compile, evaluate, global_functions, optimize
-#
-# program = compile(ast)
-# evaluate(program)  # evaluate once, to populate the function environment
-# program = optimize(program)
-# #evaluate(program)  # evaluate once, to populate the function environment
-# #program = optimize(program)
-# print(program)
-# print(evaluate(program))
-#
-# print(evaluate("(list1 '42)"))
-#
-# print(evaluate("(list2? '(1 2))"))
-#
-#
-# def undefine(name):
-#     del global_functions[name]
